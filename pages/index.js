c667448d4693cfdd7ea0bc45fd4d8e7a57fa409a
Taha <taha.iu.bd.com>
2024-04-22 21:50:22 +0600

    app to page route

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,10 @@ export default function Home() {
         <meta name='robots' content='all' />
         <meta name='google' content='nositelinkssearchbox' key='sitelinks' />
         <meta name='google' content='notranslate' key='notranslate' />
-       
       </Head>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-          Welcome by Taha
+          Welcome by Taha Pixel Added
         </p>
       </div>
 
