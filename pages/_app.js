54b027dbd60107002b0541169c0312ee6278b6e8
Taha <taha.iu.bd.com>
2024-06-16 22:34:48 +0600

    2page remove

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,7 @@ import { useState, useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { useStore } from '../store'
 import { ToastProvider } from 'react-toast-notifications'
-import { parseCookies, destroyCookie } from 'nookies'
-import { redirectUser } from '../utils/auth'
-import baseUrl from '../utils/baseUrl'
-import axios from 'axios'
+import { parseCookies } from 'nookies'
 
 import '../public/scss/bootstrap.min.css'
 import '../public/scss/animate.min.css'
@@ -61,67 +58,7 @@ export default function App({ Component, pageProps }) {
 
 App.getInitialProps = async ({ Component, ctx }) => {
   const { livani_token } = parseCookies(ctx)
-  // console.log(livani_token);
   let pageProps = {}
-
-  if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx)
-  }
-
-  if (!livani_token) {
-    // if a user not logged in then user can't access those pages
-    const isProtectedRoute =
-      ctx.pathname === '/sale-request' ||
-      ctx.pathname === '/admin/dashboard' ||
-      ctx.pathname === '/admin/products' ||
-      ctx.pathname === '/admin/orders' ||
-      ctx.pathname === '/admin/customers' ||
-      ctx.pathname === '/admin/users' ||
-      ctx.pathname === '/admin/store-requests' ||
-      ctx.pathname === '/stores/dashboard' ||
-      ctx.pathname === '/stores/products' ||
-      ctx.pathname === '/profile'
-
-    if (isProtectedRoute) {
-      redirectUser(ctx, '/')
-    }
-  } else {
-    // if a user logged in then user can't access those pages
-    const ifLoggedIn = ctx.pathname === '/signup' || ctx.pathname === '/login'
-    if (ifLoggedIn) {
-      redirectUser(ctx, '/')
-    }
-
-    try {
-      const payload = { headers: { Authorization: livani_token } }
-
-      //user
-      const url = `${baseUrl}/api/v1/auth/account`
-      const response = await axios.get(url, payload)
-      const { user, store } = response.data
-      const isSuperAdmin = user.role === 'super-admin'
-      const isAdmin = user.role === 'admin'
-
-      const isNotSuperAdmin =
-        !(isAdmin || isSuperAdmin) &&
-        (ctx.pathname === '/admin/dashboard' ||
-          ctx.pathname === '/admin/products' ||
-          ctx.pathname === '/admin/orders' ||
-          ctx.pathname === '/admin/customers' ||
-          ctx.pathname === '/admin/users' ||
-          ctx.pathname === '/admin/store-requests')
-
-      if (isNotSuperAdmin) {
-        redirectUser(ctx, '/')
-      }
-
-      pageProps.user = user
-      pageProps.store = store
-    } catch (error) {
-      console.log('error', error)
-    }
-  }
-
   return {
     pageProps,
   }
