ff697e0f5acde386a86507e2cb18f310e9af56cf
Taha <taha.iu.bd@gmail.com>
2025-07-21 21:42:42 +0600

    img

---
+++ app/img.jsx
@@ -0,0 +1,18 @@
+
+import Image from "next/image";
+
+
+
+const Img= () => {
+  return (
+    <>
+     <Image
+                            src="/acd.gif"
+                            alt="author"
+                            fill
+                          />
+    </>
+  );
+};
+
+export default Img;
